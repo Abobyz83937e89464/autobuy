de76f5a514afff7a692d4f83ba00303825aa1485
Abobyz83937e89464 <sinelniknikita41@gmail.com>
2026-07-23 02:28:51 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,27 +1,24 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.resources.Identifier;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
 	public static final String MOD_ID = "modid";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		LOGGER.info("Fabric mod initialized.");
 
-		LOGGER.info("Hello Fabric world!");
+		ClientTickEvents.END_CLIENT_TICK.register((MinecraftClient client) -> {
+			if (client.player == null) return;
+			// Здесь будет логика тиков для автобая
+		});
 	}
 
 	public static Identifier id(String path) {
